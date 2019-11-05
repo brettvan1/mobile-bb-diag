97b0fe38d49a3d7cfe2b33042fed6a3d909f3255
Brett van Gennip <50248693+brettvan1@users.noreply.github.com>
2019-11-05 16:10:19 -0500

    Update mbn-info.bat

--- mbn-info.bat
+++ mbn-info.bat
@@ -1,39 +1,39 @@
 @echo off
 
 echo "netsh mbn show capability int=*"
-echo .
+echo.
 netsh mbn show capability int=* > %userprofile%\netsh.txt
 
 echo "netsh mbn show connection int=*"
-echo .
+echo.
 netsh mbn show connection int=* >> %userprofile%\netsh.txt
 
 echo "netsh mbn show homeprovider int=*"
-echo .
+echo.
 netsh mbn show homeprovider int=* >> %userprofile%\netsh.txt
 
 echo "netsh mbn show interfaces int=*"
-echo .
+echo.
 netsh mbn show interfaces int=* >> %userprofile%\netsh.txt
 
 echo "netsh mbn show provisionedcontexts int=*"
-echo .
+echo.
 netsh mbn show provisionedcontexts int=* >> %userprofile%\netsh.txt
 
 echo "netsh mbn show radio int=*"
-echo .
+echo.
 netsh mbn show radio int=* >> %userprofile%\netsh.txt
 
 echo "netsh mbn show readyinfo int=*"
-echo .
+echo.
 netsh mbn show readyinfo int=* >> %userprofile%\netsh.txt
 
 echo"netsh mbn show signal int=*"
-echo .
+echo.
 netsh mbn show signal int=* >> %userprofile%\netsh.txt
 
 echo "netsh mbn show visibleproviders int=*"
-echo .
+echo.
 netsh mbn show visibleproviders int=* >> %userprofile%\netsh.txt
 
 notepad %userprofile%\netsh.txt
